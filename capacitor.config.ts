f7eba6c3daa418e7056cefca81a06bbb2340d417
Roberto Aguiar Alam <roberto-alam@PROCERGS.REDERS>
2023-09-05 07:21:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'video-simples',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
